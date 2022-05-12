b27995b0f9f7f25da03edb41a26aa288816f2525
DONGHEE KIM <93588200+dongh94@users.noreply.github.com>
2022-05-12 21:08:30 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 34692 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,24 +1,25 @@
 from collections import deque
 
+def bfs(N, K):
+    Q = deque([N])
+    visit = [-1] * 100001
+    visit[N] = 0
+    while Q:
+        n = Q.popleft()
 
-def bfs(N, M):
-    q = deque([N])
-    visited = [-1] * 1000001
-    visited[N] = 0
-    while q:
-        n = q.popleft()
-        if n == M:
-            return visited[n]
-        if 0 <= n - 1 < 100001 and visited[n - 1] == -1:
-            visited[n - 1] = visited[n] + 1
-            q.append(n - 1)
-        if 0 < n * 2 < 100001 and visited[n * 2] == -1:
-            visited[n * 2] = visited[n]
-            q.appendleft(n * 2)
-        if 0 <= n+1 < 100001 and visited[n + 1] == -1:
-            visited[n+1] = visited[n] + 1
-            q.append(n+1)
+        if n == K:
+            return visit[n]
 
-N, M = map(int, input().split())
-ans = bfs(N, M)
+        for m in (2*n, n-1, n+1):
+            if 0<= m <100001 and visit[m] == -1:
+                if m == 2*n:
+                    visit[m] = visit[n] + 0
+                    Q.append(m)
+                else:
+                    visit[m] = visit[n] + 1
+                    Q.append(m)
+
+
+N, K = map(int, input().split())
+ans = bfs(N, K)
 print(ans)
