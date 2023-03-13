438aed272b6d1b46652489007f31eb143574e0b1
Donghee <dongh94@naver.com>
2023-03-13 21:16:19 +0900

    스터디복습 상어초등학교 풀이완료 구현

--- 백준/Gold/구현/상어초등학교/상어초등학교.py
+++ 백준/Gold/구현/상어초등학교/상어초등학교.py
@@ -1,60 +1,56 @@
 import sys; sys.stdin=open("상어초등학교.txt")
 
-def input():
-    return sys.stdin.readline().rstrip()
-
 N = int(input())
-total = 0
-nx = [-1, 0, 1, 0]
-ny = [0, -1, 0, 1]
-stu = []
-feel = [[0 for i in range(N)] for j in range(N)]
-arr = [[0 for i in range(N)] for j in range(N)]
-save = [[] for i in range(N**2+1)]
+answer = 0
+# 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+students = []
+like_students = [[] for _ in range(N ** 2 + 1)]
 for i in range(N**2):
-    a = list(map(int, input().split()))
-    stu.append(a)
-    save[a[0]].append(a[1:])
+    info = list(map(int, input().split()))
+    students.append(info)
+    like_students[info[0]].append(info[1:])
+
+arr = [[0] * N for _ in range(N)]
 for i in range(N**2):
     temp = []
     for x in range(N):
         for y in range(N):
-            if arr[x][y] != 0:
-                continue
+            if arr[x][y] != 0: continue
             like = 0
             empty = 0
-            for z in range(4):
-                dx = x + nx[z]
-                dy = y + ny[z]
-                if dx < 0 or dx >= N or dy < 0 or dy >= N:
-                    continue
-                if  arr[dx][dy] in stu[i][1:]:
+            for d in range(4):
+                nx = x + dx[d]
+                ny = y + dy[d]
+                if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
+                if arr[nx][ny] in students[i][1:]:
                     like += 1
-                if arr[dx][dy] == 0:
+                if arr[nx][ny] == 0:
                     empty += 1
             temp.append((like, empty, (x,y)))
     temp.sort(key = lambda x : (-x[0], -x[1], x[2]))
-    arr[temp[0][2][0]][temp[0][2][1]] = stu[i][0]
+    arr[temp[0][2][0]][temp[0][2][1]] = students[i][0]
+
 for i in range(N):
     for j in range(N):
-        now = arr[i][j]
-        near = 0
+        check = arr[i][j]
+        cnt = 0
         for z in range(4):
-            dx = i + nx[z]
-            dy = j + ny[z]
-            if dx < 0 or dx >= N or dy < 0 or dy >= N:
-                continue
-            if arr[dx][dy] in save[now][0]:
-                near += 1
-        feel[i][j] = near
-for i in range(N):
-    for j in range(N):
-        if feel[i][j] == 1:
-            total += 1
-        elif feel[i][j] == 2:
-            total += 10
-        elif feel[i][j] == 3:
-            total += 100
-        elif feel[i][j] == 4:
-            total += 1000
-print(total)
+            nx = i + dx[z]
+            ny = j + dy[z]
+            if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
+            if arr[nx][ny] in like_students[check][0]:
+                cnt += 1
+
+        if cnt == 1:
+            answer += 1
+        elif cnt == 2:
+            answer += 10
+        elif cnt == 3:
+            answer += 100
+        elif cnt == 4:
+            answer += 1000
+
+print(answer)
