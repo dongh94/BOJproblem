214450813b287c4a30fc145555be777537a24722
Donghee <dongh94@naver.com>
2023-05-04 10:58:31 +0900

    change loc

--- 백준/Bronze/정리필요/1546.평균/평균.py
+++
@@ -1,9 +0,0 @@
-import sys
-n = int(sys.stdin.readline())
-l = list(map(int,sys.stdin.readline().split()))
-m = 100/max(l)
-result = []
-for i in l:
-    result.append(i*m)
-    
-print(sum(result)/n)
--- 백준/Bronze/정리필요/18108.1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-a = int(sys.stdin.readline().strip())
-print(a-543)
